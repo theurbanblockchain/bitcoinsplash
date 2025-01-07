d693c856acd5a5dbb904be6a80a39022754f23de
The Uban Blockchain <58978985+theurbanblockchain@users.noreply.github.com>
2025-01-06 18:17:23 -0800

    Update bot.php

--- apple/bot.php
+++ apple/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "8143555530:AAErxn3RdA-Z7Styz_3i0VwWl621stLA2Gg";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
